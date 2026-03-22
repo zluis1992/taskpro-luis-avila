02c15c9a837b6197bb42d9d0209240ce798c0073
Luis Avila <luis.avila@alunatek.com>
2026-03-22 16:23:58 -0500

    feat: restore jest configuration file for testing setup

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
@@ -18,4 +17,4 @@ const config: Config = {
   ],
 };
 
-export default config;
+module.exports = config;
